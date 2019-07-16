0076be57f288c1607bc4195c323259a9e6a0cc41
Egorovy4 <mailbyiwanyshyn@gmail.com>
2019-07-16 16:35:47 +0300

    First commit

---
+++ JavaAdvancedLesson04/src/main/java/javaAdvancedLesson04/UserService.java
@@ -0,0 +1,31 @@
+package javaAdvancedLesson04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+	private static UserService userService;
+
+	private UserService() {}
+
+	public static UserService getUserService() {
+		if (userService == null) {
+			userService = new UserService();
+		}
+		return userService;
+	}
+
+	List<User> listOfUsers = new ArrayList<>();
+	
+	public List<User> getListOfUsers() {
+		return listOfUsers;
+	}
+
+	public void saveUser(User user) {
+		listOfUsers.add(user);
+	}
+
+	public User getUser(String email) {
+		return listOfUsers.stream().filter(user -> user.getEmail().equals(email)).findAny().orElse(null);
+	}
+}
